176a1ab4596723c7c573467fdd2069942c7ac0c6
tekaitsu <yadykin_as@office.osu.ru>
2022-12-13 10:22:19 +0500

    Добавьте файлы проекта.

---
+++ DiagnosticsEngine/wavFiles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnosticsEngine
+{
+    class wavFiles
+    {
+        Int32 chunkID;
+    }
+}
